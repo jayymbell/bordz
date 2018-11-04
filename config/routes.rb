a0e0d2fcec4ef0ac3305a6c6cfd9af4f6be395af
Jordan Bell <jayymbell@gmail.com>
2018-11-04 09:06:53 -0600

    session routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
   get 'dashboards/show'
 
-  get 'sessions/new'
-
-  get 'sessions/create'
-
-  get 'sessions/destroy'
+  get '/login',   to: 'sessions#new'
+  post '/login',  to: 'sessions#create'
+  get '/logout',   to: 'sessions#destroy' 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
