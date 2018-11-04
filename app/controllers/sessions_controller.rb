c72712734f77b52a22b3965478b6d6e7288adb49
Jordan Bell <jayymbell@gmail.com>
2018-11-04 12:06:01 -0600

    session back-end

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,24 @@
 class SessionsController < ApplicationController
   def new
+    if logged_in?
+      redirect_to root_path
+    end
   end
 
   def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    puts user.inspect
+    if user && user.authenticate(params[:session][:password])
+      log_in(user)
+      redirect_to root_path
+    else
+      flash.now[:danger] = 'Invalid email/password combination'
+      render :new
+    end
   end
 
   def destroy
+    log_out
+    redirect_to login_path
   end
 end
