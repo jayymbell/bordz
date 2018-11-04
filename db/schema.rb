084e243b2999271eb8eb87027b548d001ab3bd98
Jordan Bell <jayymbell@gmail.com>
2018-11-04 10:56:42 -0600

    user has_secure_password

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181104151326) do
+ActiveRecord::Schema.define(version: 20181104165550) do
 
   create_table "users", force: :cascade do |t|
     t.string "first_name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20181104151326) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
