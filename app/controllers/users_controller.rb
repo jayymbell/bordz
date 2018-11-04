5e5b05781b55a72c13fbf45c51d2cd6b5025a220
Jordan Bell <jayymbell@gmail.com>
2018-11-04 11:07:21 -0600

    redirect to users path on successful create and flash notifications

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,7 +28,8 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
+        flash.now[:success] = "User was sucessfully created."
+        format.html { redirect_to users_path }
         format.json { render :show, status: :created, location: @user }
       else
         format.html { render :new }
